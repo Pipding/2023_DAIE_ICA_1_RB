106407967e659ce8a2eed0a1e218c9ad4aabfdc2
Pipding <pipding@gmail.com>
2023-12-27 17:40:03 +0000

    Add a query which fetches the duration and asset count for each project

--- sqlWorkspace/Queries.sql
+++ sqlWorkspace/Queries.sql
@@ -3,3 +3,11 @@
 -- 1.   List the total budget allocated for projects in each country, along with the count of projects per country.
 --      Display sorted by the total budget in descending order
 SELECT CustomerCountry as Country, SUM(Budget) AS 'TotalBudget', COUNT(ProjectID) AS "ProjectCount" FROM Projects JOIN Customers C on Projects.CustomerID = C.CustomerID GROUP BY CustomerCountry ORDER BY TotalBudget DESC;
+
+
+
+
+-- 2.   List the average development time for projects, categorized by the number of assets used.
+
+-- Get the asset counts
+SELECT ProjectName, (JULIANDAY(EndDate) - JULIANDAY(StartDate)) AS DaysTaken, COUNT(AssetID) AS AssetCount FROM Projects LEFT JOIN Assets A on Projects.ProjectID = A.ProjectID GROUP BY ProjectName;
